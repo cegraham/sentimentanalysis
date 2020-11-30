4d5fea25b9d617233e3b8ff6182b4c70d2f7568f
kushhp <60983239+kushhp@users.noreply.github.com>
2020-11-30 16:39:23 -0500

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -22,14 +22,12 @@ with open('2017PythonData.csv', 'r') as csv_file:
         if (nextline == True) :
             nextlines.append(line)
             nextline = False
-        #We are assuming that people use spaces properly, however some of our results hsow this isn't always the case, so this will be a limitaion to
-        #acknowledge
+        #We are assuming that people use spaces properly
         words = line.split(' ')
         for word in words:
             starting_index = word.find("https://")
             starting_index1 = word.find("http://")
             starting_index2 = word.find("www.")
-            #Need to make it work for things like .org, .edu, .gov, and others
             if (word.find(".com") != -1) :
                 ending_index = word.find(".com")
             elif (word.find(".org") != -1) :
@@ -52,7 +50,6 @@ with open('2017PythonData.csv', 'r') as csv_file:
                 startflag = True
             elif (starting_index1 != -1) :
                 starting_index += 7
-
                 startflag = True
             elif(starting_index != -1) :
                 starting_index += 8
@@ -72,17 +69,16 @@ with open('2017PythonData.csv', 'r') as csv_file:
 Counter = collections.Counter(domains)
 
 
-#This writes a csvfile-
+#This writes a output csvfile
 with open('output.csv', 'w', newline='') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(["Thread ID", "Domains", "Count"])
-    #Changing the range changes the amount of domains outputted in the output csvfile
     for i in range(1, len(domains)):
         writer.writerow([i, domains[i], Counter.get(domains[i])])
 
 # This does not output the same number of messages as domains. This is because there may be multipe URLs in a single message, however
 # theres only 1 next message for all of the URLs and so it would only do 1 message for multiple URLs in a single message. Easy to prove just
-# move the if statement into the innermost for loop, the for word in words one.
+# move the if statement into the innermost for loop, the for word in words loop.
 with open('output2.csv', 'w', newline='') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(["Thread ID", "URL line"])
